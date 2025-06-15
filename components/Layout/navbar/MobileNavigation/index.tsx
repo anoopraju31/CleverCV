5b73abe12627446f1de8493a83a3df9b49a33f05
anoop raju <anoopraju31@gmail.com>
2025-06-15 19:00:54 +0530

    Chore: Mobile Navigation added

---
+++ components/Layout/navbar/MobileNavigation/index.tsx
@@ -0,0 +1,36 @@
+import { type FC } from 'react'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
+type Props = {
+	isMenuOpen: boolean
+}
+
+const MobileNavigation: FC<Props> = ({ isMenuOpen }) => {
+	if (!isMenuOpen) return null
+
+	return (
+		<div className='md:hidden'>
+			<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white border-t'>
+				<Link href='/resume-builder' className='block px-3 py-2 text-gray-700 hover:text-indigo-600'>
+					Builder
+				</Link>
+
+				<Link href='/resume-parser' className='block px-3 py-2 text-gray-700 hover:text-indigo-600'>
+					Parser
+				</Link>
+
+				<div className='px-3 py-2 space-y-2'>
+					<Button variant='ghost' className='w-full justify-start text-gray-700'>
+						Sign In
+					</Button>
+
+					<Button className='w-full bg-indigo-600 hover:bg-indigo-700 text-white'>Get Started Free</Button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default MobileNavigation
